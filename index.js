a80384c4b6da460cd8afa2a505f0fdde2598789b
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-01-21 23:57:14 +0530

    push functioning changed

--- index.js
+++ index.js
@@ -14,17 +14,9 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => note.id === id)
-    if(note){
-        console.log(note)
-        response.json(note)
-    }
-    else{
-        response.status(404).end()
-    }
-   
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
   })
 app.delete('/api/notes/:id',(req,res)=>{
    
@@ -35,28 +27,22 @@ app.delete('/api/notes/:id',(req,res)=>{
 })
 app.post('/api/notes',(req,res)=>{
   const body=req.body
-  const generateId =()=>{
-    const maxId = notes.length > 0
-    ?Math.max(...notes.map(n=>n.id))
-    :0
-  return maxId + 1
-  }
-  if(!body.content){
+  
+  if(body.content===undefined){
     return res.status(400).json({
       error: 'content missing'
     })
   }
 
-  const note={
+  const note=new Note({
     content: body.content,
     important:body.important || false,
-    date: new Date(),
-    id: generateId()
-  }
+  
+  })
 
-  notes = notes.concat(note)
-  console.log(note)
-  res.json(note)
+  note.save().then(savedNote=>{
+    res.json(savedNote)
+  })
 })
 app.get('/api/notes',(req,res)=>{
   Note.find({}).then(notes=>{
